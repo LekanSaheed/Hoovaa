426fc6883d6a7f1d57d9cf8c8c868eec11d56e4c
unknown <lknsaheed@gmail.com>
2021-08-12 21:00:57 +0100

    some changes made

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Cards from './Cards'
+import HowItWorks from './HowItWorks'
 import SalesComponent from './SalesComponent'
 import Search from './Search'
 import WhyUs from './WhyUs'
@@ -11,6 +12,7 @@ const Home = () => {
          <Search/>
         <Cards/>
         <SalesComponent/>
+        <HowItWorks/>
         <WhyUs/>
         </>
     )
---
+++ src/components/HowItWorks.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { AiOutlineCloseCircle } from 'react-icons/ai'
+import './HowItWorks.css'
+const HowItWorks = () => {
+    const details = [
+        {
+            id: 1,
+            title: 'Check price',
+            text: 'Select your device & tell us about its current condition, our advanced Ai tech will make the perfect price for you'
+        },
+        {
+            id: 2,
+            title: 'Schedule Pickup',
+            text: 'Book a pickup, whether home or work, our personnel gets there'
+        },
+        {
+            id: 2,
+            title: 'Get Paid',
+            text: 'You get paid as soon as our personnel picks up your device'
+        }
+    ]
+    return (
+        <div>
+            <div className='how-container'>
+                <spna className='how-cont-title'>How it works</spna>
+                <>
+                   {details.map(item => {
+                       return(
+                           <div className='how-flex'>
+                               <AiOutlineCloseCircle style={{marginBottom: '-40px'}}/>
+                                {/* <img src='' alt='how'/> */}
+                    <div className='how-group'>
+                    <p><span className='how-serial'>
+                    {item.id}
+                    </span> <span style={{fontWeight: '600', fontSize: '14px', marginBottom: '4px'}}>{item.title}</span></p>
+                    <p className='how-node'>{item.text}</p>
+                    </div>
+                          </div>
+                       )
+                   })}
+                </>
+            </div>
+        </div>
+    )
+}
+
+export default HowItWorks
