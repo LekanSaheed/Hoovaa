82a59c56b2ccd84e986ffa10c265e7c1032d8a44
saheed <lknsaheed@gmail.com>
2021-08-14 15:04:18 +0100

    added icons and tweaked layout

--- src/components/HowItWorks.js
+++ src/components/HowItWorks.js
@@ -1,20 +1,26 @@
 import React from 'react'
-import { AiOutlineCloseCircle } from 'react-icons/ai'
 import './HowItWorks.css'
+import pricing from '../assets/pricing.png'
+import driver from '../assets/driver.png'
+import buying from '../assets/buying.png'
+
 const HowItWorks = () => {
     const details = [
         {
             id: 1,
+            img: pricing,
             title: 'Check price',
             text: 'Select your device & tell us about its current condition, our advanced Ai tech will make the perfect price for you'
         },
         {
             id: 2,
+            img: driver,
             title: 'Schedule Pickup',
             text: 'Book a pickup, whether home or work, our personnel gets there'
         },
         {
             id: 3,
+            img: buying,
             title: 'Get Paid',
             text: 'You get paid as soon as our personnel picks up your device'
         }
@@ -23,17 +29,20 @@ const HowItWorks = () => {
         <div>
             <div className='how-container'>
                 <span className='how-cont-title'>How it works</span>
-                <div className='how-child-flex'>
-                   {details.map(item => {
-                       return(
-                           <div className='how-flex' key={item.id}>
-                               <AiOutlineCloseCircle style={{marginBottom: '-40px'}}/>
-                                {/* <img src='' alt='how'/> */}
+                    <div className='how-child-flex'>
+                          {details.map(item => {
+                              return(
+                                      <div className='how-flex' key={item.id}>
+                                          {/* <AiOutlineCloseCircle style={{marginBottom: '-40px'}}/> */}
+                                <img src={item.img} alt='how'/>
                     <div className='how-group'>
-                    <p><span className='how-serial'>
+                    <div className='title-and-serial'>
+                     <span className='how-serial'>
                     {item.id}
-                    </span> <span style={{fontWeight: '600', fontSize: '14px', marginBottom: '4px'}}>{item.title}</span></p>
-                    <p className='how-node'>{item.text}</p>
+                    </span> 
+                      <span style={{fontWeight: '600', fontSize: '14px', marginBottom: '4px'}}>{item.title}</span>
+                    </div>
+                    <div className='how-node'>{item.text}</div>
                     </div>
                           </div>
                        )
--- src/components/WhyUs.js
+++ src/components/WhyUs.js
@@ -1,31 +1,48 @@
 import React from 'react'
 import './WhyUs.css'
-
+import sell from '../assets/sell.png'
+import professional from '../assets/professional.png'
 const WhyUs = () => {
     const details = [
         {titleText: 'One off selling',
+        img: sell,
         text: 'Sell your stuff without hassle',
         icon: ''
     },
     {titleText: 'Trained Professionals',
+    img: professional,
     text: 'Over 1000 trained personnel to get your stuff fixed',
     icon: ''
 },
-{titleText: 'One off selling',
-text: 'Sell your stuff without',
-icon: ''
+{titleText: 'Cash back guaranteed',
+text: 'If needs a refund, you can trust us on that',
+icon: '',
+img: sell,
 },
-{titleText: 'One off selling',
-text: 'Sell your stuff without',
-icon: ''
+{titleText: 'Good Conditioned gadgets',
+text: 'Ours is a home for good gadgets, what you order is what you get',
+icon: '',
+img: professional
 },
-{titleText: 'One off selling',
-text: 'Sell your stuff without',
-icon: ''
+{titleText: 'Safe and Secured transactions',
+text: 'We are partners with an highly secure payment system, so no need to fret, your money is safe',
+icon: '',
+img: professional,
 },
-{titleText: 'One off selling',
-text: 'Sell your stuff without',
-icon: ''
+{titleText: 'Warranty on Products',
+text: 'we guaranty 100% warranty on our products',
+icon: '',
+img: sell,
+},
+{titleText: 'Product insurance',
+text: 'Products come insured',
+icon: '',
+img: professional,
+},
+{titleText: 'Good customer support',
+text: 'We offer good OTL customer support',
+icon: '',
+img: sell,
 }
 
     ]
@@ -39,7 +56,7 @@ icon: ''
         {details.map((item, index) => {
             return(
                 <div key={index} className='why-us-node'>
-                    <p>{item.icon}</p>
+                    <div><img src={item.img} alt='icon'/></div>
                     <p style={{fontWeight: '500', fontSize: '14px', marginBottom: '4px'}}>{item.titleText}</p>
                     <p>{item.text}</p>
                 </div>
