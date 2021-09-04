cf1d4d12b7ad863a60253496976a95b925e60e8f
saheed <lknsaheed@gmail.com>
2021-09-04 21:40:32 +0100

    Fixed git issue on branch

---
+++ src/reducers/defaultState.js
@@ -0,0 +1,26 @@
+
+import {laptopBrands, phoneBrands, computerBrands, 
+    smartSpeakerBrands, smartWatchbrands, tvBrands, gamingGadgetBrands, cameraBrands
+} from '../components/Pages/PhoneData'
+
+const localCurrentUser = localStorage.getItem('user')
+export const defaultState = {
+    toggle: false,
+    modalContent: '',
+    isModal: false,
+    phones: [],
+    phoneBrands: phoneBrands,
+    laptopBrands: laptopBrands,
+    computerBrands: computerBrands,
+    smartSpeakerBrands: smartSpeakerBrands,
+    smartWatchbrands: smartWatchbrands,
+    tvBrands: tvBrands,
+    gamingGadgetBrands: gamingGadgetBrands,
+    cameraBrands: cameraBrands,
+    selectedDevice: [],
+    newSelected: [],
+    city: localStorage.getItem('city') ? localStorage.getItem('city') : '',
+    isCity: localStorage.getItem('city') ? false : true,
+    currentUser: localCurrentUser ? JSON.parse(localCurrentUser) : {},
+    isUser: localCurrentUser ? true : false
+}
