85275b14b200ef552575d52dd1fd749bea9f14f5
saheed <lknsaheed@gmail.com>
2021-09-26 01:00:20 +0100

    g

--- src/Admin/Repairs.js
+++ src/Admin/Repairs.js
@@ -1,10 +1,10 @@
-import { Box, Button, makeStyles, Modal, DialogContent, Dialog } from '@material-ui/core'
+import { Box, Button, makeStyles, Modal, DialogContent, Dialog, Badge } from '@material-ui/core'
 import React, {useState} from 'react'
 import { db } from '../components/firebase'
 import { GlobalContext } from '../reducers/context'
 import {AiOutlineCheck, AiOutlineCloseCircle} from 'react-icons/ai'
 import { MdKeyboardArrowRight } from 'react-icons/md'
-import { Link } from 'react-router-dom'
+import { Link, Route, Switch, useRouteMatch } from 'react-router-dom'
 
 const Repairs = () => {
     const {state} = GlobalContext()
@@ -53,6 +53,12 @@ const Repairs = () => {
         statusBad: {
             color: '#c41e3a',
             fontWeight: '600'
+        },
+        link: {
+            padding: '10px',
+            color: 'black',
+            fontWeight: '600',
+            fontSiz: '18px'
         }
     }))
     const classes = useStyle()
@@ -120,118 +126,139 @@ const Repairs = () => {
         })
         return;
     }
+    const {path, url } = useRouteMatch()
     return (
-        <Box className={classes.root}>
+       <Switch>
+           <Route path={path} exact>
+           <Box className={classes.root}>
            
-            New Repair Orders will appear here
-            {error && error}
-            <Box>
-                {loaded ? null : 'Loading'}
-                <Link><Box display='flex' justifyContent='space-between' >
-                    <span>
-                        New Repair Orders
-                    </span>
-                    <span>
-                        <MdKeyboardArrowRight/>
-                    </span></Box></Link>
-        {React.Children.toArray(
-            repairs.filter(i => i.isRepaired === false).map(repair => {
-                return(
-                    
-                    <Box className={classes.gadgets} display='flex' flexDirection='column' onLoad={() => setLoad(true)}>
-                      <div>Date: {repair.created.toDate().toDateString()}</div>
-                        <div>Gadget Name: {repair.name}</div>
-                        <div>Gadget Brand: {repair.brand}</div>
-                        <div>Gadget Model: {repair.model}</div>
-                        <div>Gadget Damages{React.Children.toArray(
-                           repair.damages && repair.damages.map(damages => {
-                                return(
-                                    <div>
-                                        {damages.value}
-                                    </div>
-                                )
-                            })
-                        )}</div>
-                        <div>Repair Status: {repair.isRepaired ?
-                         <Box className={classes.status} display='flex'  alignItems='center'>
-                             <span>Repaired</span><AiOutlineCheck/></Box> :
-                             <Box className={classes.statusBad} alignItems='center'display='flex'>
-                            <AiOutlineCloseCircle/>
-                            <span>Not Repaired</span>
-                            </Box>}</div>
+           New Repair Orders will appear here
+           {error && error}
+           <Box>
+               {loaded ? null : 'Loading'}
+               <Link to={url + '/new-repair-orders'}><Box className={classes.link} display='flex' justifyContent='space-between' >
+                   <span>
+                       <Badge badgeContent={0} color='secondary' children='New Repair Orders' showZero={true}/>
+                   </span>
+                   <span>
+                       <MdKeyboardArrowRight/>
+                   </span></Box></Link>
 
-                        <Box>
-                            <Button onClick={() => updateRepairDoc(repair)}>
-                                Change Status
-                            </Button>
-                            {repair.isRepaired &&  <Button variant='contained' color='primary' onClick={() => updateCollectedStatus(repair)}>
-                                Set Return Status
-                            </Button>}
-                        </Box>
-                        {modal && <Modal open={modal} children={<Dialog open={modal} children={<DialogContent>
-                Clicking Update Status Means the gadget is repaired and is due for collection<br/>
-                           <Box>
-                        <Button onClick={() => updateRepairDoc(repair)}>Update Status</Button> 
-                   <Button onClick={() => setModal(false)}>Cancel</Button></Box>
-            </DialogContent>}/>}/>}
-                    </Box>
-                )
-            })
-        )}
-            </Box>
+                   <Link to={url + '/repaired-gadgets'}>
+                       <Box className={classes.link} display='flex' justifyContent='space-between' >
+                   <span>
+                       <Badge badgeContent={0} color='secondary' children='Repaired Gadgets' showZero={true}/>
+                   </span>
+                   <span>
+                       <MdKeyboardArrowRight/>
+                   </span></Box></Link>
 
-            <div className={classes.tag}>Repaired</div>
-            <Box>
-        {React.Children.toArray(
-            repairs.filter(i => i.isRepaired === true).map(repair => {
-                return(
-                    <Box className={classes.gadgets} display='flex' flexDirection='column' onLoad={() => setLoad(true)}>
-                      
-                    <div>Gadget Name: {repair.name}</div>
-                    <div>Gadget Brand: {repair.brand}</div>
-                    <div>Gadget Model: {repair.model}</div>
-                    <div>Gadget Damages{React.Children.toArray(
-                       repair.damages && repair.damages.map(damages => {
-                            return(
-                                <div>
-                                    {damages.value}
-                                </div>
-                            )
-                        })
-                    )}</div>
-                    <div>Repair Status: {repair.isRepaired ?
-                     <Box className={classes.status} display='flex'  alignItems='center'>
-                         <span>Repaired</span><AiOutlineCheck/></Box> :
-                         <Box className={classes.statusBad} alignItems='center'display='flex'>
-                        <AiOutlineCloseCircle/>
-                        <span>Not Repaired</span>
-                        </Box>}</div>
+           </Box>
 
-                    <Box>
-                        
+           
+         
+       </Box>
+           </Route>
+<Route path={path + '/repaired-gadgets'}>
+<Box>
+       {React.Children.toArray(
+           repairs.filter(i => i.isRepaired === true).map(repair => {
+               return(
+                   <Box className={classes.gadgets} display='flex' flexDirection='column' onLoad={() => setLoad(true)}>
+                     
+                   <div>Gadget Name: {repair.name}</div>
+                   <div>Gadget Brand: {repair.brand}</div>
+                   <div>Gadget Model: {repair.model}</div>
+                   <div>Gadget Damages{React.Children.toArray(
+                      repair.damages && repair.damages.map(damages => {
+                           return(
+                               <div>
+                                   {damages.value}
+                               </div>
+                           )
+                       })
+                   )}</div>
+                   <div>Repair Status: {repair.isRepaired ?
+                    <Box className={classes.status} display='flex'  alignItems='center'>
+                        <span>Repaired</span><AiOutlineCheck/></Box> :
+                        <Box className={classes.statusBad} alignItems='center'display='flex'>
+                       <AiOutlineCloseCircle/>
+                       <span>Not Repaired</span>
+                       </Box>}</div>
+
+                   <Box>
+                       
+                      <Box>
+                      {repair.personnelReturned ? 'Returned' : 'Not Returned'}
+                       </Box>
+                       {/* <Button onClick={() => updateRepairDoc(repair)}>
+                           Change Status
+                       </Button><br/> */}
+
+                       {repair.isRepaired && !repair.personnelReturned ? <Button variant='contained' color='primary' 
+                       onClick={() => updateCollectedStatus(repair)}>
+                               Set Return Status
+                           </Button> : ''}
+                   </Box>
+                   {modal && <Modal open={modal} children={<Dialog open={modal} children={<DialogContent>
+           Clicking Update Status Means the gadget is repaired and is due for collection<br/>
+                      <Box>
+                   <Button onClick={() => updateRepairDoc(repair)}>Update Status</Button> 
+              <Button onClick={() => setModal(false)}>Cancel</Button></Box>
+       </DialogContent>}/>}/>}
+               </Box>
+               )
+           })
+       )}
+           </Box>
+</Route>
+           <Route path={path + '/new-repair-orders'}>
+      {React.Children.toArray(
+           repairs.filter(i => i.isRepaired === false).map(repair => {
+               return(
+                   
+                   <Box className={classes.gadgets} display='flex' flexDirection='column' onLoad={() => setLoad(true)}>
+                     <div>Date: {repair.created.toDate().toDateString()}</div>
+                       <div>Gadget Name: {repair.name}</div>
+                       <div>Gadget Brand: {repair.brand}</div>
+                       <div>Gadget Model: {repair.model}</div>
+                       <div>Gadget Damages{React.Children.toArray(
+                          repair.damages && repair.damages.map(damages => {
+                               return(
+                                   <div>
+                                       {damages.value}
+                                   </div>
+                               )
+                           })
+                       )}</div>
+                       <div>Repair Status: {repair.isRepaired ?
+                        <Box className={classes.status} display='flex'  alignItems='center'>
+                            <span>Repaired</span><AiOutlineCheck/></Box> :
+                            <Box className={classes.statusBad} alignItems='center'display='flex'>
+                           <AiOutlineCloseCircle/>
+                           <span>Not Repaired</span>
+                           </Box>}</div>
+
                        <Box>
-                       {repair.personnelReturned ? 'Returned' : 'Not Returned'}
-                        </Box>
-                        <Button onClick={() => updateRepairDoc(repair)}>
-                            Change Status
-                        </Button><br/>
-
-                        {repair.isRepaired &&  <Button variant='contained' color='primary' onClick={() => updateCollectedStatus(repair)}>
-                                Set Return Status
-                            </Button>}
-                    </Box>
-                    {modal && <Modal open={modal} children={<Dialog open={modal} children={<DialogContent>
-            Clicking Update Status Means the gadget is repaired and is due for collection<br/>
-                       <Box>
-                    <Button onClick={() => updateRepairDoc(repair)}>Update Status</Button> 
-               <Button onClick={() => setModal(false)}>Cancel</Button></Box>
-        </DialogContent>}/>}/>}
-                </Box>
-                )
-            })
-        )}
-            </Box>
-        </Box>
+                           <Button onClick={() => updateRepairDoc(repair)}>
+                               Change Status
+                           </Button>
+                           {repair.isRepaired &&  <Button variant='contained' color='primary' onClick={() => updateCollectedStatus(repair)}>
+                               Set Return Status
+                           </Button>}
+                       </Box>
+                       {modal && <Modal open={modal} children={<Dialog open={modal} children={<DialogContent>
+               Clicking Update Status Means the gadget is repaired and is due for collection<br/>
+                          <Box>
+                       <Button onClick={() => updateRepairDoc(repair)}>Update Status</Button> 
+                  <Button onClick={() => setModal(false)}>Cancel</Button></Box>
+           </DialogContent>}/>}/>}
+                   </Box>
+               )
+           })
+       )}
+      </Route>
+       </Switch>
     )
 }
 
