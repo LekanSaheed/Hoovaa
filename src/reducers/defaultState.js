c56fad35d09f9d1f7570fe85c8a7d1d98fd1a97e
saheed <lknsaheed@gmail.com>
2021-10-02 23:26:28 +0100

    h

--- src/reducers/defaultState.js
+++ src/reducers/defaultState.js
@@ -1,34 +1,47 @@
-
-import {laptopBrands, phoneBrands, computerBrands, 
-    smartSpeakerBrands, smartWatchbrands, tvBrands, gamingGadgetBrands, cameraBrands
-} from '../components/Pages/PhoneData'
-import appl from '../assets/tecnoSpark4.jpg'
-const localCurrentUser = localStorage.getItem('user')
-const sessionSR = sessionStorage.getItem('result')
-const selectedDevice = JSON.parse(localStorage.getItem('userSelected'))
+import {
+  laptopBrands,
+  phoneBrands,
+  computerBrands,
+  smartSpeakerBrands,
+  smartWatchbrands,
+  tvBrands,
+  gamingGadgetBrands,
+  cameraBrands,
+} from "../components/Pages/PhoneData";
+import appl from "../assets/tecnoSpark4.jpg";
+const localCurrentUser = localStorage.getItem("user");
+const sessionSR = sessionStorage.getItem("result");
+const selectedDevice = JSON.parse(localStorage.getItem("userSelected"));
 export const defaultState = {
-    toggle: false,
-    modalContent: '',
-    isModal: false,
-    usedGadgets: [],
-    allGadgets:
-     [{img: appl,id: 1, name: 'iphone 6', price: 45000, brand: [{value: 'apple'}], 
-     category: [{value: 'phones'}]},],
-    phoneBrands: phoneBrands,
-    laptopBrands: laptopBrands,
-    computerBrands: computerBrands,
-    smartSpeakerBrands: smartSpeakerBrands,
-    smartWatchbrands: smartWatchbrands,
-    tvBrands: tvBrands,
-    gamingGadgetBrands: gamingGadgetBrands,
-    cameraBrands: cameraBrands,
-    selectedDevice: selectedDevice ?[ selectedDevice] : [],
-    newSelected: [],
-    city: localStorage.getItem('city') ? localStorage.getItem('city') : '',
-    isCity: localStorage.getItem('city') ? false : true,
-    isMainCity: false,
-    currentUser: localCurrentUser ? JSON.parse(localCurrentUser) : {},
-    isUser: true,//localCurrentUser ? true : false,
-    searchResult: sessionSR ? JSON.parse(sessionSR) : [],
-    repairDataAdmin: []
-}
+  toggle: false,
+  modalContent: "",
+  isModal: false,
+  usedGadgets: [],
+  allGadgets: [
+    {
+      img: appl,
+      id: 1,
+      name: "iphone 6",
+      price: 45000,
+      brand: [{ value: "apple" }],
+      category: [{ value: "phones" }],
+    },
+  ],
+  phoneBrands: phoneBrands,
+  laptopBrands: laptopBrands,
+  computerBrands: computerBrands,
+  smartSpeakerBrands: smartSpeakerBrands,
+  smartWatchbrands: smartWatchbrands,
+  tvBrands: tvBrands,
+  gamingGadgetBrands: gamingGadgetBrands,
+  cameraBrands: cameraBrands,
+  selectedDevice: selectedDevice ? [selectedDevice] : [],
+  newSelected: [],
+  city: localStorage.getItem("city") ? localStorage.getItem("city") : "",
+  isCity: localStorage.getItem("city") ? false : true,
+  isMainCity: false,
+  currentUser: localCurrentUser ? JSON.parse(localCurrentUser) : {},
+  isUser: localCurrentUser ? true : false,
+  searchResult: sessionSR ? JSON.parse(sessionSR) : [],
+  repairDataAdmin: [],
+};
